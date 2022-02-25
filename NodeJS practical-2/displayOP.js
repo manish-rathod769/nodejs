789ea68a0d7ae98bab2c7168e41c91f1d17c74af
Manish Rathod <manish@manish.rathod>
2022-02-25 16:57:13 +0530

    DisplayOP.js file for displaying the output has been modififed.

--- NodeJS practical-2/displayOP.js
+++ NodeJS practical-2/displayOP.js
@@ -9,26 +9,21 @@ module.exports = (usersJSON, projectsJSON, tasksJSON) => {
         // Work with projects
         let projectToBeAppend = [];
         projectsJSON.forEach( project => {
-            let projectObj = {};
+            
             if(usersProjectID.includes(project.ID)){
-                projectObj.ID = project.ID;
-                projectObj.title = project.title;
-                projectObj.description = project.description;
-                // Wotk with tasks 
                 let taskToBeAppend = [];
                 tasksJSON.forEach( task => {
                     if(project.ID == task.projectID){
                         taskToBeAppend.push(task);
                     }
                 });
-                projectObj.tasks = taskToBeAppend;
-                projectToBeAppend.push(projectObj);
+                // console.log(taskToBeAppend)
+                project.tasks = taskToBeAppend;
+                projectToBeAppend.push(project);
             }
         });
         user.projects = projectToBeAppend;
         resultantArr.push(user);
+        console.dir(user, { depth: 4});
     });
-    resultantArr.forEach( rec => {
-        console.log(JSON.parse(JSON.stringify(rec)));
-    });
-}
+}   
