c2be628a092d783ff30c63d6cf8625782e8d92ed
Manish Rathod <manish@manish.rathod>
2022-03-03 14:56:14 +0530

    Define static file in express and use them directly.

---
+++ expressExploration/staticFiles.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use("/users", express.static(path.join(__dirname, 'usersData')));
+
+app.get('/', (req, res) => {
+    res.send("We can not access directory without making it static in express.");
+});
+
+app.listen(port, () => console.log(`Server running on http://localhost:3000`));
