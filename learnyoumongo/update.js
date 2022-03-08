cfba8e7045b3e8a280ff53a0a1ba3604f42b496c
Manish Rathod <manish@manish.rathod>
2022-03-08 19:13:06 +0530

    Update document having username tinatime in users collection

--- learnyoumongo/update.js
+++ learnyoumongo/update.js
@@ -1,10 +1,19 @@
 const { MongoClient } = require('mongodb');
-const url = `mongodb://localhost:27017/${process.argv[2]}`;
+const url = "mongodb://localhost:27017";
+const dbName = process.argv[2];
+const client = new MongoClient(url);
 
-MongoClient.connect(url, (err, db) => {
-  if (err) throw err;
-  db.collection('users').update({username: "tinatime"}, {$set: {age: 40}}, (err) => {
-    if(err) throw err;
-    db.close();
-  });
-})
+async function connection(){
+  try{await client.connect();
+  const db = client.db(dbName);
+  const col = db.collection("users");
+  const updatedData = await col.updateOne({username:"tinatime"}, {$set: {age: 40}});
+  console.log(updatedData);
+  }catch(e){
+    console.error(e);
+  }finally{
+    client.close();
+  }
+}
+
+connection();
