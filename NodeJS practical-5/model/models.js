38c12f6e149dc0bfef5d2af65b1f07d35876f867
Manish Rathod <manish@manish.rathod>
2022-03-16 18:36:39 +0530

    Schema and model modifided version 1.0.1

--- NodeJS practical-5/model/models.js
+++ NodeJS practical-5/model/models.js
@@ -32,48 +32,41 @@ const adminAndRectorSchema = new mongoose.Schema({
 });
 
 const hostelSchema = new mongoose.Schema({
-  name: {
-    type: String,
+  hostelCode: {
+    type: Number,
     required: true
   }
 });
 
 const floorSchema = new mongoose.Schema({
-  name: {
-    type: String,
+  floorCode: {
+    type: Number,
     required: true
   },
-  hostelID : {
-    type: ObjectIDDataType,
+  hostelCode : {
+    type: Number,
     required: true
   }
 });
 
 
 const roomSchema = new mongoose.Schema({
-  name: {
-    type: String,
+  roomCode: {
+    type: Number,
     required: true
   },
-  floorID: {
-    type: ObjectIDDataType,
-    required: true
-  },
-  hostelID: {
-    type: ObjectIDDataType,
+  floorCode: {
+    type: Number,
     required: true
   },
   roomCapacity: {
     type: Number,
-    required: true
-  },
-  availableSeats: {
-    type: Number,
-    required: true
+    required: true,
+    min: 1
   }
 });
 
-const studentPersonalSchema = new mongoose.Schema({
+const studentSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: true
@@ -90,44 +83,31 @@ const studentPersonalSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  allocatedRoom: {
-    type: ObjectIDDataType,
-    required: true
-  }
-});
-
-const studentAcademicSchema = new mongoose.Schema({
-  studentID: {
-    type: ObjectIDDataType,
+  allocatedRoomCode: {
+    type: Number,
     required: true
   },
-  courseName: {
-    type: String,
-    required: true
-  },
-  courseAdmissionMonth: {
-    type: String,
-    enum: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    default: 'June',
-    required: true
-  },
-  courseAdmissionYear: {
-    type: String,
-    enum: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    default: 'June',
-    required: true
-  },
-  coursePassoutMonth: {
-    type: String,
-    enum: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    default: 'June',
-    required: true
-  },
-  coursePassoutYear: {
-    type: String,
-    enum: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    default: 'May',
-    required: true
+  courseDetails: {
+    courseName: {
+      type: String,
+      required: true
+    },
+    courseAdmissionMonth: {
+      type: Number,
+      required: true
+    },
+    courseAdmissionYear: {
+      type: Number,
+      required: true
+    },
+    coursePassoutMonth: {
+      type: Number,
+      required: true
+    },
+    coursePassoutYear: {
+      type: Number,
+      required: true
+    }
   }
 });
 
@@ -155,9 +135,8 @@ const rectorModel = mongoose.model("rectors", adminAndRectorSchema);
 const hostelModel = mongoose.model("hostels", hostelSchema);
 const floorModel = mongoose.model("floors", floorSchema);
 const roomModel = mongoose.model("rooms", roomSchema);
-const studentPersonalModel = mongoose.model("studentPersonal", studentPersonalSchema);
-const studentAcademicModel = mongoose.model("studentAcademic", studentAcademicSchema);
-const studentContactModel = mongoose.model("studentContact", studentContactSchema)
+const studentModel = mongoose.model("students", studentSchema);
+// const studentContactModel = mongoose.model("studentContact", studentContactSchema)
 
 const userModel = mongoose.model('users', userSch);
 
@@ -168,7 +147,6 @@ module.exports = {
   hostelModel,
   floorModel,
   roomModel,
-  studentPersonalModel,
-  studentAcademicModel,
-  studentContactModel
+  studentModel
+  // studentContactModel
 }
