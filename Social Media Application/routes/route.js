a1067249b319c39eb106eba579c34c99a1c3b5e3
Manish Rathod <manish@manish.rathod>
2022-03-28 01:05:08 +0530

    Server.js, route.js, packet.json files modified.

--- Social Media Application/routes/route.js
+++ Social Media Application/routes/route.js
@@ -2,18 +2,35 @@ const express = require('express');
 const indexRoute = require('../controller/user.index.controller');
 
 const userRegister = require('../controller/user.register.controller');
-const userRegisterValidation = require('../utils/middleware/user.register.validator');
-
 const userLogin = require('../controller/user.login.controller');
+const userAllPost = require('../controller/user.allPost.controller');
+const userProfilePost = require('../controller/user.profilePost.controller');
+const userAddPost = require('../controller/user.addPost.controller');
+const userLogout = require('../controller/user.logout.controller');
+const userPostLike = require('../controller/user.likePost.controller');
+
+const userRegisterValidation = require('../utils/middleware/user.register.validator');
 const userLoginValidation = require('../utils/middleware/user.login.validator');
+const userAccessTokenValidation = require('../utils/middleware/user.accessToken.validator');
+const userAddPostValidation = require('../utils/middleware/user.addPost.validator');
+
+const upload = require('../utils/middleware/user.upload.image');
 
 const route = express.Router();
 
 route.get("/", indexRoute);
 
-route.get('/register', (req, res) => res.render('register'));
-route.post('/register', userRegisterValidation, userRegister);
+route.get('/register', (req, res) => res.render('register', {errorMessage: ""}));
+route.get('/login', (req, res) => res.render('login', {errorMessage: ""}));
+route.get('/add', userAccessTokenValidation, (req, res) => res.render('addPost', {errorMessage: ""}));
+route.get('/all', userAccessTokenValidation, userAllPost);
+route.get('/profile', userAccessTokenValidation, userProfilePost);
+route.get('/logout', userAccessTokenValidation, userLogout);
 
+route.post('/register', userRegisterValidation, userRegister);
+route.post('/login', userLoginValidation, userLogin);
+route.post('/add', upload.single('image'), userAddPostValidation, userAddPost);
+route.post('/like', userAccessTokenValidation, userPostLike)
 // route.get('/login', (req, res) => res.render('login'));
 // route.post('/login', userLoginValidation, userLogin);
 
--- Social Media Application/server.js
+++ Social Media Application/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-
+const cookieParser = require('cookie-parser');
+const connectToDB = require('./db/connection');
 
 // Require .env file
 require('dotenv').config();
@@ -11,12 +12,18 @@ const portNum = process.env.PORT;
 // Setting View Engine
 app.set("view engine", "ejs");
 
+// Connecting to database
+connectToDB();
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json()); 
+
+app.use(cookieParser());
 // Load Assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
-app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
+app.use('/images', express.static(path.resolve(__dirname, "db/Images")));
 
 app.use("/", require("./routes/route"));
 
-
-app.listen(8080, () => console.log(`Server runnig on http://localhost:${portNum}/`));
+app.listen(portNum, () => console.log(`Server runnig on http://localhost:${portNum}/`));
