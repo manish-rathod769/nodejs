2372b63899dc83c83908935ebc54a8b9a85371d0
Manish Rathod <manish@manish.rathod>
2022-03-29 18:35:44 +0530

    MiddleWare function has been modified.

---
+++ Social Media Application/utils/middleware/user.accessToken.middleware.js
@@ -0,0 +1,21 @@
+const { verify } = require('jsonwebtoken');
+const userModel = require('../../model/users/users.model');
+
+const userAccessTokenMiddleWare = async (req, res, next) => {
+  try{
+    const accessToken = req.cookies.accessToken;
+    if(!accessToken) throw new Error('Please Login First !!!');
+    const payload = verify(accessToken, process.env.ACCESS_TOKEN_SECRET); //Return Data od user. Here it will return emailID.
+    
+    // Check If data exist in database or not
+    const matchedUser = await userModel.find({email: payload.emailID});
+    
+    if(!matchedUser[0]) throw new Error('Data does not exist !!!');
+    if(matchedUser[0].accessToken != accessToken) throw new Error('Please Login First !!!');
+    next()
+  }catch(e){
+    res.render('login', { errorMessage: e.message });
+  }
+}
+
+module.exports = userAccessTokenMiddleWare;
